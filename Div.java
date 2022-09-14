db8acb39f67d807bbc9bac8f224a2138765c83ad
sanyog-chavhan <sanyogchavhan2016@gmail.com>
2022-09-14 22:31:21 +0530

    Added Division Functionality

---
+++ Div.java
@@ -0,0 +1,8 @@
+public class Div {
+    public static void main(String... args) {
+        int a = 10;
+        int b = 5;
+        int c = a/b;
+        System.out.print("Division : "+ c);
+    }
+}
